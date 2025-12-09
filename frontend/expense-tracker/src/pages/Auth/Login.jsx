c6cdd5fbe2586bc12cac199b0ed6b67ed87e79b0
code-Prakash <prakashkumar959903@gmail.com>
2025-12-09 10:59:20 +0530

    fist the apis

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState,useContext } from 'react';
 import AuthLayout from '../../components/layouts/AuthLayout';
 import { Link, useNavigate } from 'react-router-dom';
 import Input from "../../components/Inputs/input";
@@ -7,67 +7,67 @@ import axiosInstance from "../../utils/axiosInstance";
 import { API_PATHS } from "../../utils/apiPaths";
 import { UserContext } from '../../context/UserContext';
 
+
 const Login = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
   const { updateUser } = useContext(UserContext);
+
   const navigate = useNavigate();
 
+  // Handle login form submit
   const handleLogin = async (e) => {
     e.preventDefault();
 
-    // Basic validations
     if (!email) {
       setError("Please enter your email");
       return;
     }
     if (!validateEmail(email)) {
-      setError("Please enter a valid email address");
+      setError('Please enter a valid email address');
       return;
     }
+
     if (!password) {
-      setError("Please enter a password");
+      setError('Please enter a password');
       return;
     }
 
     setError('');
 
+    // Login API Call
     try {
-      // Login API call
-      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, { email, password });
-      console.log("LOGIN RESPONSE:", response.data);
+      const response = await axiosInstance.post(API_PATHS.AUTH.LOGIN, {
+        email,
+        password
+      });
 
-      const { token, user } = response.data;
+      console.log("LOGIN RESPONSE:", response.data);
+      const { token , user } = response.data;
 
       if (token) {
-        // Save token in localStorage
         localStorage.setItem("token", token);
-
-        // Update context
         updateUser(user);
-
-        // Navigate to dashboard
         navigate("/dashboard");
-      } else {
-        setError("Login failed: token missing");
       }
-    } catch (err) {
+    }
+    catch (err) {
       if (err.response && err.response.data.message) {
         setError(err.response.data.message);
       } else {
         setError("Something went wrong. Please try again later.");
       }
     }
-  };
+  }
 
   return (
     <AuthLayout>
-      <div className="lg:w-[70%] md:h-full flex flex-col justify-center">
+      <div className='lg:w-[70%] md:h-full flex flex-col justify-center'>
         <h3 className="text-xl font-semibold text-black">Welcome Back</h3>
-        <p className="text-xs text-gray-700 mt-2 mb-6">
-          Please enter your details to login
+        <p className='text-xs text-gray-700 mt-2 mb-6'>
+          Please Enter your details to login
         </p>
 
         <form onSubmit={handleLogin}>
@@ -75,34 +75,34 @@ const Login = () => {
             value={email}
             onChange={({ target }) => setEmail(target.value)}
             label="Email Address"
-            placeholder="example123@gmail.com"
-            type="text"
+            placeholder='example123@gmail.com'
+            type='text'
           />
 
           <Input
             value={password}
             onChange={({ target }) => setPassword(target.value)}
             label="Password"
-            placeholder="Min 8 characters"
-            type="password"
+            placeholder='Min 8 characters'
+            type='password'
           />
 
-          {error && <p className="text-red-500 text-xs pb-2.5">{error}</p>}
+          {error && <p className='text-red-500 text-xs pb-2.5'>{error}</p>}
 
-          <button className="btn-primary" type="submit">
+          <button className='btn-primary' type='submit'>
             LOGIN
           </button>
 
-          <p className="text-[13px] text-slate-800 mt-3">
+          <p className='text-[13px] text-slate-800 mt-3'>
             Don't have an account?{" "}
-            <Link className="font-medium text-violet-600 underline" to="/signup">
+            <Link className='font-medium text-violet-600 underline' to="/signup">
               Sign Up
             </Link>
           </p>
         </form>
       </div>
     </AuthLayout>
-  );
-};
+  )
+}
 
 export default Login;
